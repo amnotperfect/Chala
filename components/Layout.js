86149e5b7a2f954cb16c6fb5c319db640bc35a14
amntoperfect <amnotperfect981@gmail.com>
2022-07-05 13:49:58 +0300

    added manifest file

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,6 @@
 import styles from "../styles/Layout.module.css";
 import Head from "next/head";
+import Link from "next/link";
 
 const Layout = ({ children }) => {
   return (
@@ -8,10 +9,8 @@ const Layout = ({ children }) => {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="whitesmoke" />
-        <meta
-          name="description"
-          content="Welcome to Chala secondary "
-        />
+        <meta name="description" content="Welcome to Chala secondary " />
+        <link rel="manifest" href="/manifest.json" />
         <title>Chala secondary</title>
       </Head>
       <main>{children}</main>
